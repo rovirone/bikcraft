f022bbdd7f7bb3c24daf65cd573aecc17308071f
rovirone <roney0101@hotmail.com>
2022-07-15 21:57:53 -0300

    Atualização JS

--- js/formulario.js
+++ js/formulario.js
@@ -3,10 +3,10 @@ const formulario = document.querySelector("form");
 function formularioEnviado(resposta) {
   if (resposta.ok) {
     formulario.innerHTML =
-      "<p class='font-2-l' style ='grid-columm: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #317a00;'>Mensagem enviada</span>, em breve entraremos em contato. Geralmente respondemos em 24 horas</p>";
+      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #317A00;'>Mensagem enviada</span>, em breve entraremos em contato. Geralmente respondemos em 24 horas.</p>";
   } else {
     formulario.innerHTML =
-      "<p class='font-2-l' style ='grid-columm: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #e00000;'>Erro no envio</span>, você pode enviar diretamente para o nosso email em: roney@rovirone.com.br</p>";
+      "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #E00000;'>Erro no envio</span>, você pode enviar diretamente para o nosso email em: contato@bikcraft.com</p>";
   }
 }
 
@@ -16,7 +16,14 @@ function enviarFormulario(event) {
   botao.disabled = true;
   botao.innerText = "Enviando...";
 
-  const data = new FormData(formulario);
+  const campos = formulario.querySelectorAll("[name]");
+  const data = new FormData();
+
+  campos.forEach((campo) => {
+    const name = campo.getAttribute("name");
+    const value = campo.value;
+    data.append(name, value);
+  });
 
   fetch("./enviar.php", {
     method: "POST",
@@ -24,4 +31,4 @@ function enviarFormulario(event) {
   }).then(formularioEnviado);
 }
 
-formulario.addEventListener("submit");
+formulario.addEventListener("submit", enviarFormulario);
